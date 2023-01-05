45f8524a6f202c88912761c6f7275a5b5c87f4d3
GSantagati <gs@414customweb.com>
2023-01-05 12:02:51 -0600

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,21 @@
+// Options the user could type in
+const prompts = [
+  ["hi", "hello"],
+  ["I want to plan a trip"],
+  ["Italy", "France", "Greece", "Spain", "Germany", "Switzerland", "Austria", "Belgium", "Bulgaria", "Croatia", "Cyprus", "Czech Republic", "Denmark", "Estonia", "Finland", "France", "Germany", "Greece", "Hungary", "Ireland", "Italy", "Latvia", "Lithuania", "Luxembourg", "Malta", "Netherlands", "Poland", "Portugal", "Romania", "Slovakia", "Slovenia", "Spain", "Sweden", "Switzerland", "Turkey", "UK", "US"],
+  
+];
+  
+  // responses to the user's input
+  const replies = [
+    ["Hello, how can I help you?"],
+    ["Great, I can help you with that. Where would you like to go?"],
+    ["Where would you like to go?"],
+  ];
+
+  
+  const alternative = [
+    ["Great, I can help you with that. Where would you like to go?"],
+    ["Where would you like to go?"],
+    ["That's a beautiful place. I'd love to go there too. Let me ask Giovanni to give you a call so we can start planning your trip. Need anything else?"],
+  ];
